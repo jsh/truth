de27994baf19bd407c21e03393282f71b049a037
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-09-12 14:25:02 -0600

    Handle a description, specify bits, wild_type.
    Factor out, and pass in, the description,
    so I can use the same argument parser for different programs.
    
    Permit specifying bits, either individually or as a range.
    Permit specifying a wild_type, for programs that do mutation.
    
    Rename parse_args() -> get_args() to avoid confusion
    with argparse.ArgumentParser().parse_args() and parse_args.py

--- parse_args.py
+++ parse_args.py
@@ -1,29 +1,67 @@
 #!/usr/bin/env python3
-"""Parse arguments testably.
-Modelled on https://bit.ly/35u38gV
-"""
+"""Argument handling."""
 
 import argparse
+import re
 import sys
+from pathlib import Path
 
 
-def parse_args(args):
-    """The argument parser
-    :param list[str] args:
-    :returns: parsed args
+def truth():
+    """An important default.
+    :returns: Path to truth
+    :rtype: pathlib.Path
+    """
+    true = Path("true")
+    if not Path("/bin" / true).is_file():
+        true = Path("/usr/bin" / true)
+    return true
+
+
+def get_args(description, args=None):
+    """Parse the args
+    :param str description: description message for executable
+    :returns: the args, massaged and sanity-checked
     :rtype: argparse.Namespace
     """
-    parser = argparse.ArgumentParser(description="Parse some arguments")
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--bit", help="bit(s) of interest")
+    parser.add_argument("--wild_type", default=truth(), help="un-mutated executable")
     parser.add_argument("--verbose", help="be extra chatty", action="store_true")
-    return parser.parse_args(args)
+
+    args = parser.parse_args(args)
+
+    assert Path(args.wild_type).is_file(), "No file {}".format(args.wild_type)
+    args.size = Path(args.wild_type).stat().st_size * 8
+    args.start, args.end = parse_span(args.bit)
+    if args.end == sys.maxsize:
+        args.end = args.size
+    return args
 
 
-def main():
-    """The big top."""
-    args = parse_args(sys.argv[1:])
-    if args.verbose:
-        print("See? It worked.")
-
-
-if __name__ == "__main__":
-    main()
+def parse_span(bit):
+    """parse a span
+    :param str bit: range specified
+    :returns: beginning and end of span
+    :rtype: tuple(int, int)
+    """
+    span = r"(\d*):(\d*)"
+    if not bit:
+        bit = "0:"  # the entire sequence
+    if re.fullmatch(span, bit):
+        # pull apart start and end
+        match = re.fullmatch(span, bit)
+        start = match.group(1)
+        if not start:
+            start = "0"
+        start = int(start)
+        end = match.group(2)
+        end = int(end) if end else sys.maxsize
+    elif re.fullmatch(r"\d*", bit):
+        start, end = (int(bit), int(bit) + 1)
+    else:
+        raise TypeError(
+            "--bit argument must be a colon-separated range or a single int"
+        )
+    return (start, end)
--- t/test_parse_args.py
+++ t/test_parse_args.py
@@ -3,26 +3,57 @@
 Modelled on https://bit.ly/35u38gV
 """
 
+import os
 import sys
 
-from parse_args import parse_args
+import pytest
+
+from parse_args import get_args, parse_span, truth
+
+
+def test_truth():
+    truthpath = truth()
+    assert os.system(truthpath) == 0
+
+
+def test_get_args_defaults():
+    """get_args defaults"""
+    parser = get_args("Testing wild_type")
+    assert parser.wild_type == truth()
+
+
+def test_bit():
+    """get_args understands --verbose"""
+    parser = get_args("Testing verbose", ["--bit=69"])
+    assert parser.start == 69
+
+
+def test_wild_type():
+    """get_args understands --wild_type"""
+    parser = get_args("Testing wild_type", ["--wild_type=/usr/bin/false"])
+    assert parser.wild_type == "/usr/bin/false"
 
 
 def test_verbose():
-    """parse_args understands --verbose"""
-    parser = parse_args(["--verbose"])
+    """get_args understands --verbose"""
+    parser = get_args("Testing verbose", ["--verbose"])
     assert parser.verbose
 
 
-def test_capsys(capsys):
-    """Sample capsys.
-    From https://docs.pytest.org/en/stable/capture.html
-    """
-    print("hello")
-    sys.stderr.write("world\n")
-    captured = capsys.readouterr()
-    assert captured.out == "hello\n"
-    assert captured.err == "world\n"
-    print("next")
-    captured = capsys.readouterr()
-    assert captured.out == "next\n"
+def test_help():
+    """test help which throws a SystemExit"""
+    # TODO: How do I do this?
+
+
+def test_parse_span():
+    """parse_span handles all five span types"""
+    assert (0, sys.maxsize) == parse_span("")
+    assert (69, 70) == parse_span("69")
+    assert (6, 9) == parse_span("6:9")
+    assert (69, sys.maxsize) == parse_span("69:")
+    assert (0, 69) == parse_span(":69")
+    assert (0, sys.maxsize) == parse_span(":")
+    # TODO: need a mock to set args.size
+
+    with pytest.raises(TypeError):
+        parse_span("xxx")
