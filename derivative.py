643482792a762e4a9d1d71232a86a1747760aed0
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-09-21 15:51:26 -0600

    Report a running diff.
    On really big input files,
    don't wait until the diff finishes to report,

--- derivative.py
+++ derivative.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
-"""Record change points."""
+"""Record change points.
+Given an input file that is a list of paired values, (x, y),
+Report every line whose y value
+doesn't equal that of the preceeding line,
+and a count of the subsequent lines with that y value
+
+For example:
+    1 a
+    2 a
+    3 a
+    5 b
+    4 b
+would be reported as
+    3 ['1', 'a']
+    2 ['4', 'b']
+"""
 
 import sys
 
-lines = sys.stdin.readlines()
-
-values = [line.rstrip() for line in lines]
-values.append("This is a sentine!")  # a sentinel
-
-last_value = None
+last_value = [None, None]
 last_index = -1
-for index, value in enumerate(values):
-    if value != last_value:
-        span = index - last_index
-        if index > 0:
-            print(last_value, span)
-        last_value = value
-        last_index = index
+index = -1
+with open(sys.argv[1]) as fin:
+    for line in fin:
+        index += 1
+        value = line.split()
+        if value[1] != last_value[1]:
+            span = index - last_index
+            if index > 0:
+                print(span, last_value)
+            last_value = value
+            last_index = index
+index += 1
+span = index - last_index
+print(span, last_value)
