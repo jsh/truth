4e49bf9e4f98e0e281ce0b0c4243cb21dc899929
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-09-21 15:19:36 -0600

    Document some errors.

--- run.py
+++ run.py
@@ -5,6 +5,20 @@ import shlex
 import subprocess
 from subprocess import PIPE
 from typing import Tuple
+"""
+1 - Catchall for general errors
+2 - Misuse of shell builtins (according to Bash documentation)
+126 - Command invoked cannot execute
+127 - “command not found”
+128 - Invalid argument to exit
+128+n - Fatal error signal “n”
+130 - Script terminated by Control-C
+-1 - Same as 254 == -1 mod 256
+255\* - Exit status out of range
+
+See https://tldp.org/LDP/abs/html/exitcodes.html
+See also /usr/include/sysexits.h
+"""
 
 
 def run(command: str, timeout: int = 1) -> Tuple[int, str, str, str]:
