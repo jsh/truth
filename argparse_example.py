13175cda7da660b01b807c16c30cb60ee766370a
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-09-24 18:03:33 -0600

    Add argument validation.

--- argparse_example.py
+++ argparse_example.py
@@ -2,9 +2,14 @@
 """Argument handling."""
 
 import argparse
+import collections
 import sys
+from pathlib import Path
 from typing import List, Optional
+
 from utils import parsed_span, which
+
+Span = collections.namedtuple("Span", "start end")
 
 
 def get_args(
@@ -16,8 +21,6 @@ def get_args(
     :returns: the args, massaged and sanity-checked
     :rtype: argparse.Namespace
     """
-
-    # TODO: I can do a lot of argument validation with "type="
 
     parser = argparse.ArgumentParser(description=description)
     bits_or_bytes = parser.add_mutually_exclusive_group()
@@ -35,7 +38,29 @@ def get_args(
 
     parsed_args = parser.parse_args(args)
     print(parsed_args)
-    # do some more arg manipulation and validation
+
+    # argument validatation and manipulation
+    # TODO: I could do file work with type=, too.
+    assert Path(parsed_args.wild_type).is_file(), f"No file {parsed_args.wild_type}"
+
+    parsed_args.size = Path(parsed_args.wild_type).stat().st_size * 8
+
+    assert (
+        parsed_args.bit ^ parsed_args.byte
+    ), "Specify either bits or bytes, but not both."
+    if parsed_args.bits:
+        bytes_start = parsed_args.bits.start // 8
+        if parsed_args.bits.end == sys.maxsize:
+            parsed_args.bits.end = parsed_args.size
+        bytes_end = parsed_args.bits.end // 8
+        parsed_args.bytes = Span(bytes_start, bytes_end)
+    elif parsed_args.bytes:
+        bits_start = parsed_args.bytes.start * 8
+        if parsed_args.bytes.end == sys.maxsize:
+            bits_end = parsed_args.size
+        parsed_args.bytes.end = bits_end // 8
+        parsed_args.bits = Span(bits_start, bits_end)
+
     return parsed_args
 
 
