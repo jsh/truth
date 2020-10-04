10d36262cf959e1e2d842f483aa7369b324cd34e
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-10-04 16:53:25 -0600

    snapshot

--- parse_args.py
+++ parse_args.py
@@ -2,15 +2,59 @@
 """Argument handling."""
 
 import argparse
+import atexit
 import collections
+import shutil
 import sys
 import tempfile
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from utils import parsed_span, which
 
 Span = collections.namedtuple("Span", "start end")
+
+
+def bit_and_byte_ranges(parsed_args: argparse.Namespace) -> Tuple[Span, Span]:
+    """
+    :param argparse.Namespace parsed_args: parsed args
+    :returns: bit- and byte-ranges
+    :rtype: Tuple[Span, Span]
+    Note that parse_args() prevents both bits and bytes from being set
+    """
+    if parsed_args.bits:
+        bits_start, bits_end = parsed_args.bits
+        bits_end = min(bits_end, parsed_args.size_in_bits)
+        bytes_start = bits_start // 8
+        bytes_end = (bits_end // 8) + 1
+    elif parsed_args.bytes:
+        bytes_start, bytes_end = parsed_args.bytes
+        bytes_end = min(bytes_end, parsed_args.size_in_bytes)
+        bits_start = bytes_start * 8
+        bits_end = (bytes_end) * 8
+    else:  # specifying neither means "the whole Zoon"
+        bits_start, bits_end = 0, parsed_args.size_in_bits
+        bytes_start, bytes_end = 0, parsed_args.size_in_bytes
+
+    return Span(bits_start, bits_end), Span(bytes_start, bytes_end)
+
+
+def output_paths(parsed_args: argparse.Namespace):
+    """
+    some damn string
+    :param argparse.Namespace parsed_args: parsed args
+    """
+    if parsed_args.mutant:  # name file to keep it in a particular place
+        basepath = Path(parsed_args.mutant)
+        dirpath = basepath.parent
+    elif parsed_args.mutants:  # random filename in named directory
+        dirpath = Path(parsed_args.mutants)
+        basepath = Path(tempfile.NamedTemporaryFile(dir=parsed_args.mutants).name)
+    else:  # one or more temporary files in temporary directory
+        dirpath = Path(tempfile.mkdtemp())
+        atexit.register(shutil.rmtree, dirpath)  # cleanup on exit
+        basepath = None
+    return basepath, dirpath
 
 
 def get_args(
@@ -42,10 +86,9 @@ def get_args(
         help="un-mutated executable (default: %(default)s)",
     )
     parser.add_argument("--verbose", help="be extra chatty", action="store_true")
-    parser.add_argument(
-        "--mutants",
-        help="directory to store mutants"
-    )
+    mutant_or_mutants = parser.add_mutually_exclusive_group()
+    mutant_or_mutants.add_argument("--mutant", help="file to store mutant")
+    mutant_or_mutants.add_argument("--mutants", help="directory to store mutants")
     assert description, "executable description required"
     if args is None:
         args = []
@@ -57,27 +100,12 @@ def get_args(
     # attribute validatation and enhancement
     assert Path(parsed_args.wild_type).is_file(), f"No file {parsed_args.wild_type}"
 
-    # sizes
+    # bits and bytes
     parsed_args.size_in_bytes = Path(parsed_args.wild_type).stat().st_size
     parsed_args.size_in_bits = parsed_args.size_in_bytes * 8
+    parsed_args.bits, parsed_args.bytes = bit_and_byte_ranges(parsed_args)
 
-    # bit- and byte-ranges
-    # Note that parse_args() prevents both bits and bytes from being set
-    if parsed_args.bits:
-        bits_start, bits_end = parsed_args.bits
-        bits_end = min(bits_end, parsed_args.size_in_bits)
-        bytes_start = bits_start // 8
-        bytes_end = (bits_end // 8) + 1
-    elif parsed_args.bytes:
-        bytes_start, bytes_end = parsed_args.bytes
-        bytes_end = min(bytes_end, parsed_args.size_in_bytes)
-        bits_start = bytes_start * 8
-        bits_end = (bytes_end) * 8
-    else:  # specifying neither means "the whole Zoon"
-        bits_start, bits_end = 0, parsed_args.size_in_bits
-        bytes_start, bytes_end = 0, parsed_args.size_in_bytes
-
-    parsed_args.bits = Span(bits_start, bits_end)
-    parsed_args.bytes = Span(bytes_start, bytes_end)
+    # mutant paths
+    parsed_args.basepath, parsed_args.dirpath = output_paths(parsed_args)
 
     return parsed_args
